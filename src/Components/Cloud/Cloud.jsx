12d7900493c57e47b34056ced5e678a10da98cfb
Bastien Cochet <bastien@MacBook-de-Bast.local>
2023-10-31 18:42:01 +0100

    feature/feat: new component cloud and drag and drop state apply to clouds

--- src/Components/Cloud/Cloud.jsx
+++ src/Components/Cloud/Cloud.jsx
@@ -1,13 +1,60 @@
+import React, { useState, useEffect, useRef } from 'react';
 import './Cloud.css';
 
+function Cloud() {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const cloudRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
 
-function Cloud () {
+    useEffect(() => {
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        const cloudWidth = cloudRef.current.offsetWidth;
+        const cloudHeight = cloudRef.current.offsetHeight;
+
+        const randomX = Math.random() * (windowWidth - cloudWidth);
+        const randomY = Math.random() * (windowHeight - cloudHeight);
+
+        setPosition({ x: randomX, y: randomY });
+    }, []); // Le tableau vide signifie que cela se produit seulement une fois, après le montage initial du composant.
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setOffset({
+            x: e.clientX - position.x,
+            y: e.clientY - position.y
+        });
+    };
+
+    const handleMouseMove = (e) => {
+        if (isDragging) {
+            setPosition({
+                x: e.clientX - offset.x,
+                y: e.clientY - offset.y
+            });
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    };
+
     return (
-        <div className='container-cloud'>
-            <div className='cloud'></div>
-            <div className='cloud2'></div>
-        </div>
-    )
-};
+        <div
+            ref={cloudRef}
+            className='cloud'
+            style={{
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+                position: 'absolute'
+            }}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+        ></div>
+    );
+}
 
 export default Cloud;
+
